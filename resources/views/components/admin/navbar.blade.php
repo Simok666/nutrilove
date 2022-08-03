2f959a5946b81ed1a486287865ef4342f9c4d25c
MasThor <thaariqprs97@gmail.com>
2022-08-03 14:40:55 +0700

    Remove comment navbar

--- resources/views/components/admin/navbar.blade.php
+++ resources/views/components/admin/navbar.blade.php
@@ -3,62 +3,6 @@
         <div class="sidebar-menu">
             <ul class="menu">
                 <li class='sidebar-title'>Main Menu</li>
-                {{-- <li class="sidebar-item  ">
-                    <a href="/admin" class='sidebar-link'>
-                        <i data-feather="home" width="20"></i>
-                        <span>Dashboard</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a href="#" class='sidebar-link'>
-                        <i data-feather="users" width="20"></i>
-                        <span>Admin</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a href="#" class='sidebar-link'>
-                        <i data-feather="users" width="20"></i>
-                        <span>User</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a href="#" class='sidebar-link'>
-                        <i data-feather="file" width="20"></i>
-                        <span>Articles</span>
-                    </a>
-                </li>
-                <li class="sidebar-item has-sub">
-                    <a href="#" class='sidebar-link'>
-                        <i data-feather="droplet" width="20"></i>
-                        <span>Gizi</span>
-                    </a>
-                    <ul class="submenu active">
-                        <li>
-                            <a href="/admin/setting/landing-page">Riwayat Gizi Remaja</a>
-                        </li>
-                        <li>
-                            <a href="/admin/setting/landing-page">Riwayat Gizi Anak</a>
-                        </li>
-                        <li>
-                            <a href="/admin/setting/landing-page">Riwayat Gizi Balita</a>
-                        </li>
-                        <li>
-                            <a href="/admin/setting/landing-page">Settings</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="sidebar-item active has-sub">
-
-                    <a href="#" class='sidebar-link'>
-                        <i data-feather="settings" width="20"></i>
-                        <span>Setting</span>
-                    </a>
-                    <ul class="submenu active">
-                        <li>
-                            <a href="/admin/setting/landing-page">Landing Page</a>
-                        </li>
-                    </ul>
-                </li> --}}
                 @foreach ($menu ?? '' as $label => $item)
                     <li class="sidebar-item  {{ empty($item['childs']) ? '' : 'has-sub' }}">
                         <a href="{{ $item['url'] }}"
