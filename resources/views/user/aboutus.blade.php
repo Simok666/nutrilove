adc1e36b4657245992b8b7427d29d5123a556558
Rachmad Fidhani <simokart3@gmail.com>
2022-09-19 20:44:15 +0700

    Kari su

--- resources/views/user/aboutus.blade.php
+++ resources/views/user/aboutus.blade.php
@@ -1,50 +1,54 @@
 <x-user.layout title="Index">
     
-    <!-- ======= About Section ======= -->
-    <section id="about" class="about">
-      <div class="container">
+  <!-- ======= About Section ======= -->
+  <section id="about" class="about">
+    <div class="container">
 
-        <div class="row justify-content-between">
-          <div class="col-lg-5 d-flex align-items-center justify-content-center about-img">
-            <img src="user_assets/img/about-img.svg" class="img-fluid" alt="" data-aos="zoom-in">
-          </div>
-          <div class="col-lg-6 pt-5 pt-lg-0">
-            <h3 data-aos="fade-up">About us</h3>
-            <p data-aos="fade-up" data-aos-delay="100">
-              Informasi Tentang Kami dan Team Kami
-            </p>
-            <div class="row">
-              <div class="col-md-6" data-aos="fade-up" data-aos-delay="100">
-                <i class="bx bx-receipt"></i>
-                <h4>Tentang Kami</h4>
-                <p> NutriLove beridiri pada tahun 2022 dengan tujuan sebagai media edukasi bagi masyarakat luas.</p>
-                <p> platform Kami berfokus pada informasi terkait stunting yang masih menjadi masalah gizi utama di Indonesia.</p>
-                <p> Kami berharap informasi yang kami sediakan dapat membantu anda untuk hidup lebih sehat. </p>
-                <p> Bersama kita cegah stunting. Eat Well, Life Healty, Good Body. </p>
-                
-              </div>
-              <div class="col-md-6" data-aos="fade-up" data-aos-delay="200">
-                <i class="bx bx-user-plus"></i>
-                <h4>Tim Kami</h4>
-                <p>1. Nike Noviana Putri</p>
-                <p>2. Larissa Dian Gayatri</p>
-                <p>3. Bunga Putri Arindra</p>
-                <p>4. Riza Amru Salzabila</p>
-                <p>5. Dhiah Widyaningtyas Budi</p>
-                <p>6. Sigita Ardelia Fista</p>
-                <p>7. Bayu Puspita</p>
-                <p>Thanks to:</p>
-                <p>1. Bapak Bastinus Doddy, SKM., MM</p>
-                <p>2. Bapak Hasan Aroni, SKM., MPH</p>
-                <p>3. Bapak Sugeng Iwan Setyobudi, STP., M.Ke. </p>
-                <p>4. Bapak Ibnu Fajar, SKM., M.Kes., RD </p>
-                <p>5. Bapak Juin Hadisuyitno, SST., M.Kes </p>
-              </div>
+      <div class="row justify-content-between">
+        <div class="col-lg-5 d-flex align-items-center justify-content-center about-img">
+          <img src="user_assets/img/about-img.svg" class="img-fluid" alt="" data-aos="zoom-in">
+        </div>
+        <div class="col-lg-6 pt-5 pt-lg-0">
+          <h3 data-aos="fade-up">About us</h3>
+          <p data-aos="fade-up" data-aos-delay="100">
+            Informasi Tentang Kami dan Team Kami
+          </p>
+          <div class="row">
+            <div class="col-md-6" data-aos="fade-up" data-aos-delay="100">
+              <i class="bx bx-receipt"></i>
+              <h4>Tentang Kami</h4>
+              <p> NutriLove beridiri pada tahun 2022 dengan tujuan sebagai media edukasi bagi masyarakat luas.</p>
+              <p> platform Kami berfokus pada informasi terkait stunting yang masih menjadi masalah gizi utama di Indonesia.</p>
+              <p> Kami berharap informasi yang kami sediakan dapat membantu anda untuk hidup lebih sehat. </p>
+              <p> Bersama kita cegah stunting. Eat Well, Life Healty, Good Body. </p>
+              
+            </div>
+            <div class="col-md-3" data-aos="fade-up" data-aos-delay="200">
+              <i class="bx bxs-user-detail"></i>
+              <h4>Tim Kami :</h4>
+              <p>1. Nike Noviana Putri</p>
+              <p>2. Larissa Dian Gayatri</p>
+              <p>3. Bunga Putri Arindra</p>
+              <p>4. Riza Amru Salzabila</p>
+              <p>5. Dhiah Widyaningtyas Budi</p>
+              <p>6. Sigita Ardelia Fista</p>
+              <p>7. Bayu Puspita</p>
+              
+            </div>
+            <div class="col-md-3" data-aos="fade-up" data-aos-delay="200">
+            <i class="bx bx-user-plus"></i>
+            <h4>Thanks to :</h4>
+              <p>1. Bapak Bastinus Doddy, SKM., MM</p>
+              <p>2. Bapak Hasan Aroni, SKM., MPH</p>
+              <p>3. Bapak Sugeng Iwan Setyobudi, STP., M.Ke. </p>
+              <p>4. Bapak Ibnu Fajar, SKM., M.Kes., RD </p>
+              <p>5. Bapak Juin Hadisuyitno, SST., M.Kes </p>
             </div>
           </div>
         </div>
-
       </div>
-    </section><!-- End About Section -->
+
+    </div>
+  </section><!-- End About Section -->
 
 </x-user.layout>
